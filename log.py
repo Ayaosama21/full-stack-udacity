948834ace9da8890fd8cf49b10ae491c8810585f
Aya Osama Abdelaziz <36484232+Ayaosama21@users.noreply.github.com>
2019-09-06 16:37:25 +0200

    Update log.py

--- log.py
+++ log.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3
-#to connect the database
+# !/usr/bin/env python3
+# to connect the database
 
-import psycopg2        
+import psycopg2
 DBNAME = "news"
+
+
 # the most popular three articles of all times
 def most_popular__three__article():
     db = psycopg2.connect(database=DBNAME)
@@ -13,21 +15,24 @@ def most_popular__three__article():
         on articles.ID = authors.ID
         group by articles
         ORDER BY articleCount DESC;""")
+
     return c.fetchall()
     db.close()
 
 # the most popular article authors of all time
+
+
 def most_popular_article_authors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("""SELECT AuthorName , count(*) as authorsCount
+    c.execute("""SELECT authors.name , count(*) as authorsCount
         from authors
         join articles
         on articles.ID = authors.ID
-        group by AuthorName
+        group by authors
         order by authorsCount desc;""")
     out = c.fetchall()
-    for author , num in out:
+    for author, num in out:
         print(" \"{}\" -- {} views".format(author, num))
     print("-------------------------")
 
@@ -39,31 +44,24 @@ def errors_more_than_1_precentage():
     c.execute("""select * from
                  (select date(time),round(100.0*sum(case log.status
                   when '200 OK'  then 0 else 1 end)
-                  /count(log.status),3)
-                   as error from log 
-                   group by date(time) 
-                   order by error desc) as
-                    subq where 
-                    error > 1;""")
+                  /count(log.status),3) as error from log group by
+                  date(time) order by error desc) as subq where error > 1;""")
     return c.fetchall()
     db.close()
-
-
-
 # display first query
+
+
 def dis_first():
     dis_articles = most_popular__three__article()
     for name, num in dis_articles:
         print(" \"{}\" -- {} views".format(name, num))
-
-
 # display third query
+
+
 def dis_third():
     dis_errors = errors_more_than_1_precentage()
     for day, percentage in dis_errors:
         print("{0:%B %d, %Y} -- {1:.2f} % errors".format(day, percentage))
-
-
 
 if __name__ == '__main__':
     dis_first()
